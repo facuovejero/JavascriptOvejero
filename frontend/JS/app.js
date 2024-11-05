7f238600d05ed19b9187695ae52d7c54232c1f7e
Facundo Ovejero <facuovejero017@gmail.com>
2024-11-05 14:56:49 -0300

    clase 1

---
+++ frontend/JS/app.js
@@ -0,0 +1,7 @@
+const primerNombre = prompt("Pon tu nombre:");
+const segundoNombre = prompt("Pon tu apellido:");
+
+const resultado = primerNombre + segundoNombre;
+alert("Bienvenido " + resultado);
+console.log("Tu resultado es:" + resultado);
+debugger
