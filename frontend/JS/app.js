9213ca18737ca2c5407744ffe450c30c1f3ebace
Facundo Ovejero <facuovejero017@gmail.com>
2024-11-05 21:03:34 -0300

    Preentrega1

--- frontend/JS/app.js
+++ frontend/JS/app.js
@@ -1,7 +1,17 @@
-const primerNombre = prompt("Pon tu nombre:");
-const segundoNombre = prompt("Pon tu apellido:");
-
-const resultado = primerNombre + segundoNombre;
-alert("Bienvenido " + resultado);
-console.log("Tu resultado es:" + resultado);
+let adivinarNumero = parseInt(prompt("Adivina el numero del 1 al 10"));
 debugger
+while (adivinarNumero < 12 ){
+    debugger;
+    switch (adivinarNumero){
+    case 9:
+        alert ("Felicitaciones, Adivinaste!");
+        break;
+    case 11:
+        alert ("Te dije del 1 al 10")
+        break;
+    default: 
+        alert("Sigue intentando adivinar");
+        break;
+    }
+    adivinarNumero = parseInt(prompt("Adivina el numero del 1 al 10"));
+}
